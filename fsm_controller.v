e0ae0903f1a2d9e7392a0e9976aefb6a636fed9a
marcelochincha <marcelo.chincha@utec.edu.pe>
2023-11-20 17:07:56 -0500

    Update fsm_controller.v

--- fsm_controller.v
+++ fsm_controller.v
@@ -1,10 +1,8 @@
-module fsm_controller(clk,next,dat,result,flags);
+module fsm_controller(clk,next,dat,result);
     input clk,next;
     input [15:0]dat;
     output [15:0] result;
-    output [3:0]flags;  
-
-    reg [1:0] state, nextstate;
+    reg[1:0] state, nextstate;
 
     reg[15:0] A,B;
     reg[2:0] operation;
@@ -12,38 +10,44 @@ module fsm_controller(clk,next,dat,result,flags);
     parameter setA = 2'b01;
     parameter setB = 2'b10;
     parameter calc = 2'b11;
+    
+    wire[15:0] buff;
 
     //INSTANCIATE ALU HERE
-    alu aluInstance(A,B,operation,result,flags);
-
-
+    alu aluInstance(A,B,operation,buff);
+    
     // state register
-    always @ (posedge clk)
+    always @ (posedge clk) begin
         state <= nextstate;
-
-
-    always @ (*) // next state logic
+    end
+    
+    always @ (*) begin // next state logic
         case (state)
             getOperator: 
-                if (next) nextstate = setA;
-                else nextstate = getOperator;
+                if (next) begin 
+                    nextstate <= setA;
+                    operation <= dat[2:0];
+                end
+                else nextstate <= getOperator;
             setA:
                 if (next) begin
-                    nextstate = setB;
-                    A = dat;
+                    nextstate <= setB;
+                    A <= dat;
                 end
-                else nextstate = setA;
+                else nextstate <= setA;
             setB:
                 if (next) begin
-                    nextstate = calc;
-                    B = dat;
+                    nextstate <= calc;
+                    B <= dat;
                 end
-                else nextstate = setB;
+                else nextstate <= setB;
             calc:
-                if (next) nextstate = getOperator;
-                else nextstate = calc;
-
+                if (next) nextstate <= getOperator;
+                else nextstate <= calc;
+            default:
+                nextstate <= getOperator;
         endcase
-        // output logic
-        assign result = {16{(state == calc)}} && result ;
+     end
+             
+     assign result = {16{(state == calc)}} && buff; 
 endmodule
