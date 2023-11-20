59a7131911af84aa0eaf3e972879e17b89693562
Irán María Alvarez Flores <91108632+iranalvarez27@users.noreply.github.com>
2023-11-20 12:59:38 -0500

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,40 +1,43 @@
-module alu (input [31:0] a,b,
-            input [2:0] ALUControl,
-            output reg [31:0] Result, //assign always block
-            output wire [3:0] ALUFlags); //explicit wire for assign with {}
-  
-  wire negative, zero, carry, overflow; // define wire for each flag (n,z,c,v)
-  wire [32:0] sum;
-  
-  
-  assign sum = a + (ALUControl[0]? ~b: b) + ALUControl[0]; //ADDER: two's complement
-  
-  /*
-  ALUControl Logic
-  00: sum
-  01: sub
-  10: and
-  11: or
+module alu (
+    input [7:0] a,
+    input [7:0] b,
+    input [2:0] ALUControl,
+    output reg [7:0] Result,
+    output wire [3:0] ALUFlags
+);
 
-  */
-  always @(*)
-    casex (ALUControl[2:0]) //case, casex, casez
-      3'b00?: Result = sum;
-      3'b010: Result = a & b;
-      3'b011: Result = a | b;
-      3'b100: Result = a << b; // LSL
-      3'b101: Result = a >> b; // LSR 
-      default: Result = 32'b0;
-    endcase
-  
- //flags: result -> negative, zero
-  assign negative = Result[31];
-  assign zero = (Result == 32'b0);
-  //flags: additional logic -> v, c
-  assign carry = (ALUControl[1]==1'b0) & sum[32];
-  assign overflow = (ALUControl[1]==1'b0) & ~(a[31] ^ b[31] ^ ALUControl[0]) & (a[31] ^ sum[31]);
+    wire negative, zero, carry, overflow; // define wire for each flag (n, z, c, v)
+    wire [8:0] sum;
+    wire [7:0] multiplier_result;
 
-  assign ALUFlags = {negative, zero, carry, overflow};
+    // Instantiate the fmul module
+    fmul uut_mult (
+        .A(a),
+        .B(b),
+        .result(multiplier_result)
+    );
+
+    // ALU and multiplier functionality
+    always @* begin
+        case (ALUControl[2:0])
+            3'b000: Result = a + (ALUControl[0] ? ~b : b) + ALUControl[0]; // ADDER: two's complement
+            3'b111: Result = multiplier_result[7:0]; // Assuming you want the lower 8 bits of the multiplier result
+            3'b010: Result = a & b;
+            3'b011: Result = a | b;
+            3'b100: Result = a << b; // LSL
+            3'b101: Result = a >> b; // LSR 
+            default: Result = 8'b0;
+        endcase
+    end
+
+    // Flags: result -> negative, zero
+    assign negative = Result[7];
+    assign zero = (Result == 8'b0);
+
+    // Flags: additional logic -> v, c
+    assign carry = (ALUControl[1] == 1'b0) & sum[8];
+    assign overflow = (ALUControl[1] == 1'b0) & ~(a[7] ^ b[7] ^ ALUControl[0]) & (a[7] ^ sum[7]);
+
+    assign ALUFlags = {negative, zero, carry, overflow};
 
 endmodule
-
