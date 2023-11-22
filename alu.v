7a0a273eee5d2d641eae1964c3e4dc61d660cdfa
marcelochincha <marcelo.chincha@utec.edu.pe>
2023-11-22 12:19:22 -0500

    multiplier and alu implementation. Check for aluoperator code

--- alu.v
+++ alu.v
@@ -3,8 +3,7 @@ module alu (
     input [15:0] b,
     input [2:0] ALUControl,
     output reg [15:0] Result,
-    output wire [3:0] ALUFlags
-   
+    output wire [3:0] ALUFlags   
 );
 
     wire negative, zero, carry, overflow; // define wire for each flag (n,z,c,v)
@@ -13,7 +12,7 @@ module alu (
 
     assign condinvb = ALUControl[0] ? ~b : b;
     assign sum = a + condinvb + ALUControl[0];
-    wire multiplier_result;
+    wire[15:0] multiplier_result;
 
     // Instantiate the fmul module
     fmul uut_mult (
@@ -24,25 +23,23 @@ module alu (
 
     // ALU and multiplier functionality
     always @* begin
-        case (ALUControl[2:0])
-            3'b000: Result = a + (ALUControl[0] ? ~b : b) + ALUControl[0]; // ADDER: two's complement
-            3'b111: Result = multiplier_result; // Assuming you want the lower 8 bits of the multiplier result
+        casex (ALUControl[2:0])
+            3'b00?: Result = a + (ALUControl[0] ? ~b : b) + ALUControl[0]; // ADDER: two's complement
             3'b010: Result = a & b;
             3'b011: Result = a | b;
             3'b100: Result = a << b; // LSL
             3'b101: Result = a >> b; // LSR 
-            default: Result = 8'b0;
+            3'b111: Result = multiplier_result; // Assuming you want the lower 8 bits of the multiplier result
+            default: Result = 16'b0;
         endcase
     end
 
     // Flags: result -> negative, zero
     assign negative = Result[15];
-    assign zero = (Result == 8'b0);
-
+    assign zero = (Result == 16'b0);
     // Flags: additional logic -> v, c
     assign carry = (ALUControl[1] == 1'b0) & sum[16];
     assign overflow = (ALUControl[1] == 1'b0) & ~(a[15] ^ b[15] ^ ALUControl[0]) & (a[15] ^ sum[15]);
-
     assign ALUFlags = {negative, zero, carry, overflow};
 
 endmodule
